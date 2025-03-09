c02b8e0464115085ff9ce0caa482281e51834a7b
Anuj Raj <akgupta.anuj@gmail.com>
2025-03-10 01:33:29 +0530

    Initialize backend with Express, MongoDB, and environment configuration

---
+++ Backend/app.js
@@ -0,0 +1,18 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
+const app = express();
+
+const connectToDb = require('./db/db');
+connectToDb();
+
+const cors = require('cors');
+
+app.use(cors());
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+
+module.exports = app;
